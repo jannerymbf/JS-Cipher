78ef77dc82ad4e2c294dc36595304d4fe2c46e81
Jannery Briceño <jannerymai@gmail.com>
2023-03-29 14:21:59 -0500

    Add decode method

--- src/cipher.js
+++ src/cipher.js
@@ -12,6 +12,21 @@ const cipher = {
     })
 
     return result.join('');
+  },
+
+  decode: (offset, message) => {
+    const result = [];
+    const messageArray = message.split('');
+
+    messageArray.forEach(letter => {
+
+      let codeForUpperCase = ((letter.charCodeAt(0) - 65 + 26 - parseInt(Math.abs(offset))) % 26) + 65;
+      let codeForLowerCase = ((letter.charCodeAt(0) - 97 + 26 - parseInt(Math.abs(offset))) % 26) + 97;
+
+      (letter.charCodeAt(0) <= 90 && letter.charCodeAt(0) >= 65) ? result.push(String.fromCharCode(codeForUpperCase)) : (letter.charCodeAt(0) <= 122 && letter.charCodeAt(0) >= 97) ? result.push(String.fromCharCode(codeForLowerCase)) : result.push(letter);
+    })
+
+    return result.join('');
   }
 };
 
