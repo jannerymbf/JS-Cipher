74d3a8a0f115ef7fab7f24c428f7fbe5bd4e49bd
Jannery Briceño <jannerymai@gmail.com>
2023-03-29 14:20:52 -0500

    Add encode method

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,18 @@
 const cipher = {
-  // ...
+  encode: (offset, string) => {
+    const result = [];
+    let stringArray = string.split('');
+
+    stringArray.forEach(letter => {
+
+      let codeForUpperCase = ((letter.charCodeAt(0) - 65 + parseInt(Math.abs(offset))) % 26) + 65;
+      let codeForLowerCase = ((letter.charCodeAt(0) - 97 + parseInt(Math.abs(offset))) % 26) + 97;
+
+      (letter.charCodeAt(0) <= 90 && letter.charCodeAt(0) >= 65) ? result.push(String.fromCharCode(codeForUpperCase)) : (letter.charCodeAt(0) <= 122 && letter.charCodeAt(0) >= 97) ? result.push(String.fromCharCode(codeForLowerCase)) : result.push(letter);
+    })
+
+    return result.join('');
+  }
 };
 
 export default cipher;
