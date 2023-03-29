551802e6182a724bf72ef4ca0eb767c0bbc01aeb
Jannery Briceño <jannerymai@gmail.com>
2023-03-29 17:51:46 -0500

    Add interaction to buttons code and decode

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+// Calling variables from html
+
+const message = document.getElementById('message');
+const offset = document.getElementById('offset');
+const codeBtn = document.getElementById('code');
+const decodeBtn = document.getElementById('decode');
+const result = document.getElementById('result');
+
+codeBtn.addEventListener('click', () => {
+  result.innerHTML = cipher.encode(offset.value, message.value);
+})
+
+decodeBtn.addEventListener('click', () => {
+  result.innerHTML = cipher.decode(offset.value, message.value);
+})
