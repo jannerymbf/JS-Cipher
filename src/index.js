fd9f94ee3a8b71aa8f60d02ad0be358d6ed3c216
Jannery Briceño <jannerymai@gmail.com>
2023-04-01 13:28:24 -0500

    Add validations and their styles

--- src/index.js
+++ src/index.js
@@ -6,12 +6,30 @@ const message = document.getElementById('message');
 const offset = document.getElementById('offset');
 const codeBtn = document.getElementById('code');
 const decodeBtn = document.getElementById('decode');
-const result = document.getElementById('result');
+const result = document.getElementById('output');
 
 codeBtn.addEventListener('click', () => {
-  result.innerHTML = cipher.encode(offset.value, message.value);
+  if(message.value.length === 0 || offset.value.length === 0) {
+    result.style.color = 'red';
+    result.innerHTML = "Try again! Don't forget to enter your message and offset."
+    
+  } else {
+    result.style.color = 'var(--main-light-blue)';
+    result.innerHTML = cipher.encode(offset.value, message.value);
+    message.value = '';
+    offset.value = '';
+  }
 })
 
 decodeBtn.addEventListener('click', () => {
-  result.innerHTML = cipher.decode(offset.value, message.value);
+  if(message.value.length === 0 || offset.value.length === 0) {
+    result.style.color = 'red';
+    result.innerHTML = "Try again! Don't forget to enter your message and offset."
+    
+  } else {
+    result.style.color = 'var(--main-light-blue)';
+    result.innerHTML = cipher.decode(offset.value, message.value);
+    message.value = '';
+    offset.value = '';
+  }
 })
